698d24ebe193c2e650530b7e98da3b1f5fcdd4d9
federico <pxorg@puertodelacruz.org>
2015-09-07 18:33:18 +0100

    Primer Commit

---
+++ Primero/src/primero/Federico.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primero;
+
+/**
+ *
+ * @author federico
+ */
+public class Federico {
+    public Federico(){
+        System.out.println(this.getClass().getSimpleName());
+    }
+}
---
+++ Primero/src/primero/Primero.java
@@ -0,0 +1,9 @@
+package primero;
+
+public class Primero {
+
+    public static void main(String[] args) {
+        Federico cero=new Federico();
+    }
+    
+}
